ca2140add143ddcc612d9500d8264a7e65306ec8
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-18 15:48:03 +0300

    finished with lab09

--- src/components/Weather07.js
+++ src/components/Weather07.js
@@ -1,72 +1,25 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Weather07 extends Component {
-
-  state = {
-    cityInfo: [],
-    error: "",
-  };
-
-  getCityData = (e) => {
-    e.preventDefault();
-    console.log("getCityData");
-
-    let cityName = e.target.cityname.value;
-    let serverUrl = process.env.REACT_APP_SERVER;
-    let url = `${serverUrl}/weather?searchQuery=${cityName}`;
-
- 
-    axios
-      .get(url)
-      .then((data) => {
-        this.setState({ cityInfo: data.data, error: "" });
-        console.log(this.state.cityInfo);
-      })
-      .catch((error) => {
-        this.setState({
-          error: "There is an error , choose another city name",
-          cityInfo: null,
-        });
-      });
-  };
-
   render() {
     return (
       <div className="body">
-     
-
-        <Form onSubmit={this.getCityData} className="form">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>City Name:</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="write city name"
-              name="cityname"
-            />
-          </Form.Group>
-          <input type="submit" className="btn btn-primary" />
-        </Form>
-
-        {this.state.cityInfo ? (
+        {this.props.cityInfo ? (
           <div className="result">
-            {this.state.cityInfo.map((item) => {
+            {this.props.cityInfo.map((item) => {
               return (
                 <div className="weatherData">
-                  <p> Description : {item.description}</p>
-                  <p> valid_date :{item.valid_date}</p>
+                  <p key={item.id}> Description : {item.description}</p>
+                  <p key={item.id}> valid_date :{item.valid_date}</p>
                 </div>
               );
             })}
           </div>
         ) : (
-          <p className="error"> ATTENTION{this.state.error}</p>
+          <p className="error"> ATTENTION {this.props.error}</p>
         )}
-        
       </div>
     );
   }
 }
-
